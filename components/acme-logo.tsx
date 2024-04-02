1634b3208ffa3b5a6918bb01f5715ab220cb2b5e
“Quique” <quiquecampos92@outlook.com>
2024-04-02 10:55:35 +0200

    añadir skeletons a todas las paginas

--- components/acme-logo.tsx
+++ components/acme-logo.tsx
@@ -4,11 +4,15 @@ import Image from 'next/image';
 
 export default function AcmeLogo({ isColumn = false }: { isColumn?: boolean }) {
   return (
-    <div className={`${lusitana.className} flex ${isColumn ? "flex-col" : "flex-row"} ${isColumn ? "items-start" : "items-center"} leading-none text-[#EB8833]`}>
-      <div className={`hidden md:block`}>
+    <>
+    <div className={`${lusitana.className} flex ${isColumn ? "flex-col items-center" : "flex-row items-center"} leading-none text-[#EB8833]`}>
+      <div className={`hidden md:block ${isColumn ? "mb-2" : "mr-2"}`}> {/* Agregar mb-2 si es columna, mr-2 si es fila */}
         <Image src="/moneyhub.png" width={80} height={80} alt="MoneyHub" />
       </div>
-      <p className="text-[44px]">MoneyHub</p>
     </div>
+      <div className={`${lusitana.className} flex ${isColumn ? "flex-col" : "flex-row"} ${isColumn ? "items-start" : "items-center"} leading-none text-[#EB8833]`}>
+        <p className="text-[44px] ">MoneyHub</p>
+      </div>
+    </>
   );
 }
